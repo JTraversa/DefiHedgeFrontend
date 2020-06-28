78f26460012a77c60003bb997c7f784e8bc850d7
Julian Traversa <juliant@nescience.io>
2020-06-28 18:26:58 -0400

    Update Header.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -60,7 +60,7 @@ export default function Header(props) {
     [classes.absolute]: absolute,
     [classes.fixed]: fixed
   });
-  const brandComponent = <Button className={classes.title}><img src={image1} /></Button>;
+  const brandComponent = <Button className={classes.title}><img src={image1} width="300px" height="180px" /></Button>;
   return (
     <AppBar className={appBarClasses}>
       <Toolbar className={classes.container}>
