34da875578544a77d39e80dd97882fdefd218b3b
Julian Traversa <juliant@nescience.io>
2020-06-28 12:35:26 -0400

    test
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -29,7 +29,7 @@ export default function HeaderLinks(props) {
       <ListItem className={classes.listItem}>
         <CustomDropdown
           noLiPadding
-          buttonText="Components"
+          buttonText="Markets"
           buttonProps={{
             className: classes.navLink,
             color: "transparent"
@@ -37,14 +37,14 @@ export default function HeaderLinks(props) {
           buttonIcon={Apps}
           dropdownList={[
             <Link to="/" className={classes.dropdownLink}>
-              All components
+              ETH1MONTH
             </Link>,
             <a
-              href="https://creativetimofficial.github.io/material-kit-react/#/documentation?ref=mkr-navbar"
+              href="https://nescience.io"
               target="_blank"
               className={classes.dropdownLink}
             >
-              Documentation
+              DAI1MONTH
             </a>
           ]}
         />
