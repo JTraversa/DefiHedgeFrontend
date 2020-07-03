9fd722541c248a7bebfc654b89437eacbcfd6130
Julian Traversa <juliant@nescience.io>
2020-07-03 01:15:40 -0400

    Update Components.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -48,7 +48,7 @@ export default function Components(props) {
         }}
         {...rest}
       />
-      <Parallax image={require("assets/img/3420834.eps")}>
+      <Parallax image={require("assets/img/3420834.svg")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem>
