207793f39136eca2d7331539d6bdf4f6a7169baa
Julian Traversa <juliant@nescience.io>
2020-07-03 13:01:01 -0400

    Update material-kit-react.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/assets/jss/material-kit-react.js
+++ src/assets/jss/material-kit-react.js
@@ -160,7 +160,9 @@ const title = {
   margin: "1.75rem 0 0.875rem",
   textDecoration: "none",
   fontWeight: "700",
-  fontFamily: `"Roboto Slab", "Times New Roman", serif`
+  fontFamily: `"Roboto Slab", "Times New Roman", serif`,
+  transform: `translate(100px, 200px)
+  
 };
 
 const cardTitle = {
