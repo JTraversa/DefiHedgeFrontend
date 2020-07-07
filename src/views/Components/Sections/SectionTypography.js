afd516f8185429e31cff48d2292d228c51148d60
Julian Traversa <juliant@nescience.io>
2020-07-07 00:24:37 -0400

    Update SectionTypography.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/Components/Sections/SectionTypography.js
+++ src/views/Components/Sections/SectionTypography.js
@@ -30,7 +30,7 @@ export default function SectionTypography() {
         <div className={classes.space50} />
         <div>
           <div className={classes.title}>
-            <h2>Protect yourself from rate volatility</h2>
+            Protect yourself from rate volatility
           </div>
           <br />
           <GridContainer>
@@ -81,7 +81,7 @@ export default function SectionTypography() {
         </div>
 		<div>
           <div className={classes.title}>
-            <h2>Leverage your capital & increase your exposure to high rates</h2>
+            Leverage your capital & increase your exposure to high rates
           </div>
           <br />
           <GridContainer>
