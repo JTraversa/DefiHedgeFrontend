0f76d056ca186160e646e83690fc097f26f124a6
Julian Traversa <juliant@nescience.io>
2020-07-11 00:54:11 -0400

    Img
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/Components/Sections/SectionCarousel.js
+++ src/views/Components/Sections/SectionCarousel.js
@@ -10,7 +10,7 @@ import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
 
-import BuiltUsing from "assets/img/BuiltUsing.png";
+import BuiltUsing from "assets/img/BuiltUsing2.png";
 import image2 from "assets/img/bg2.jpg";
 import image3 from "assets/img/bg3.jpg";
 
