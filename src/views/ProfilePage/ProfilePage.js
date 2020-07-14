53cbca2fe6fe5ee2061de496f8ec41c344c5a219
Julian Traversa <juliant@nescience.io>
2020-07-14 11:56:38 -0400

    Update ProfilePage.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -60,14 +60,14 @@ export default function ProfilePage(props) {
       />
       <Parallax small filter image={require("assets/img/svgversion.svg")} />
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <div>
-		    <GridContainer justify="center">
+			<GridContainer justify="center">
               <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
 			  <div className={classes.name}>
               <h3 className={classes.title}>How It Works</h3>
               </div>
               </GridItem>
             </GridContainer>
+        <div>
           <div className={classes.container}>
 			<GridContainer justify="center">
 			  <GridItem xs={12} sm={6} className={classes.navWrapper}>
