d1c36082ba7ac7035d4d802c4a181f578c29a1a0
Julian Traversa <juliant@nescience.io>
2020-07-14 12:36:52 -0400

    small
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -59,12 +59,12 @@ export default function ProfilePage(props) {
         {...rest}
       />
       <Parallax small filter image={require("assets/img/svgversion.svg")} />
-      <div className={classNames(classes.main, classes.mainRaised)}>
-			<GridContainer justify="center">
+	  <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
               <h3 className={classes.title}>How It Works</h3>
               </GridItem>
             </GridContainer>
+      <div className={classNames(classes.main, classes.mainRaised)}>
         <div>
           <div className={classes.container}>
 			<GridContainer justify="center">
