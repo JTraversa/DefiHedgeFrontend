89f9df66153a5c854b48742f19da18bee73c987a
Julian Traversa <juliant@nescience.io>
2020-07-15 10:55:23 -0400

    Update ProfilePage.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -85,10 +85,12 @@ export default function ProfilePage(props) {
 			  </GridItem>
 			  </div>
 			</GridContainer>
+			<br />
+			<br />
 			<GridContainer justify="center">
 			  <GridItem xs={12} sm={6} className={classes.navWrapper}>
 				<div className={classes.name}>
-				Much like any traditional exchange, rates are determined by a marketplace of buyers & sellers (floating & fixed sides).
+				Much like any traditional exchange, rates are determined by a marketplace of buyers & sellers (Fixed & Floating respectively).
 				</div>
 				<div>{" "}</div>
 				<div className={classes.name}>
