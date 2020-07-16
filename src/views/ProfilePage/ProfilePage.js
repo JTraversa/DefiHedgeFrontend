4ccb7c5082e3026b379ce0b5f47813c28e07f845
Julian Traversa <juliant@nescience.io>
2020-07-15 21:53:01 -0400

    Update ProfilePage.js
    Co-Authored-By: Julian Traversa <jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -78,8 +78,8 @@ export default function ProfilePage(props) {
 				<div className={classes.name}>
 				Users can enter limit orders at a given rate, or market orders to take the best rate currently available.
 				</div>
-				</GridItem>
 				</div>
+				</GridItem>
 			  </div>
 			  <GridItem xs={12} sm={6} className={classes.navWrapper}>
 				<div className={classes.imgcontainer}>
