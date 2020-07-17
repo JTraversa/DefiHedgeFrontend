77e1c6cc8017428174c54be3eaf426ec69908cc4
Julian Traversa <juliant@nescience.io>
2020-07-17 13:56:01 -0400

    1
    Co-Authored-By: Julian Traversa <62613746+jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -215,7 +215,7 @@ export default function ProfilePage(props) {
 		<div className={classes.container2}>
 			<GridContainer justify="center">
 			  <GridItem xs={12} sm={6} className={classes.navWrapper}>
-			  <Card carousel2>
+			  <Card carousel>
               <Carousel {...settings}>
                 <div className={classes.container2}>
 					<GridContainer justify="center">
