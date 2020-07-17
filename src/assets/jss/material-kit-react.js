8ce9606af5815057daa941aa7e62687cd84344ba
Julian Traversa <juliant@nescience.io>
2020-07-17 12:15:09 -0400

    Update material-kit-react.js
    Co-Authored-By: Julian Traversa <62613746+jtraversa@users.noreply.github.com>

--- src/assets/jss/material-kit-react.js
+++ src/assets/jss/material-kit-react.js
@@ -64,7 +64,7 @@ const card = {
 };
 
 const defaultFont = {
-  fontFamily: '"Geneva", "Tahoma", "Verdana", sans-serif',
+  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
   fontWeight: "300",
   lineHeight: "1.5em"
 };
