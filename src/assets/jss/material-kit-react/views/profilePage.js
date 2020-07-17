f6f2ca641932766cfd78c41331800aa4b090f7e8
Julian Traversa <juliant@nescience.io>
2020-07-17 10:03:52 -0400

    Update profilePage.js
    Co-Authored-By: Julian Traversa <62613746+jtraversa@users.noreply.github.com>

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -47,8 +47,7 @@ const profilePageStyle = {
   name: {
     marginTop: "20px",
 	transform: "translate(-10px,0px)",
-	color: "black",
-	font: "Raleway"
+	color: "black"
   },
   topmargin: {
 	  marginTop: "20px"
@@ -62,7 +61,7 @@ const profilePageStyle = {
   mainRaised: {
     margin: "-60px 30px 0px",
     borderRadius: "6px",
-	backgroundColor: "#b6bdb5",
+	backgroundColor: "#bac2bc",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   },
