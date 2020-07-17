a9c810850e7b5e82578ea20ff6ff1ce24d5c7e82
Julian Traversa <juliant@nescience.io>
2020-07-16 23:48:59 -0400

    Update profilePage.js
    Co-Authored-By: Julian Traversa <62613746+jtraversa@users.noreply.github.com>

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -25,7 +25,7 @@ const profilePageStyle = {
   img: {
 	  width: "100%",
       height: "auto",
-	  borderRadius: "25%",
+	  borderRadius: "15%",
 	  boxShadow:
       "0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   },
