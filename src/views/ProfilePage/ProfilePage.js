27dfecac05906e856359fa87ab7da19bf082c8f4
Julian Traversa <juliant@nescience.io>
2020-07-17 21:37:07 -0400

    Update ProfilePage.js

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -212,7 +212,7 @@ export default function ProfilePage(props) {
 		</div>
 		
 		<Divider light="true" />
-		<div className={classes.container2}>
+		<div className={classes.container3}>
 			<GridContainer justify="center">
 			  <GridItem xs={12} sm={6} className={classes.navWrapper}>
 			  <Card carousel2>
