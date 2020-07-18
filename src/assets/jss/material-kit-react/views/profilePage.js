a46626c14df5a95af7c453b8a7d2e137cf6f886c
Julian Traversa <juliant@nescience.io>
2020-07-17 23:47:54 -0400

    Update profilePage.js

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -22,7 +22,7 @@ const profilePageStyle = {
 	  marginBottom: "40px"
   },
   container3: {
-	  paddingRight: "15px",
+	  paddingRight: "0px",
 	  paddingLeft: "15px",
 	  marginRight: "auto",
 	  marginLeft: "auto",
