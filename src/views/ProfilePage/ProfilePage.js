d01061c96a9ceaa8624adf8bf280a54c0b261a25
Julian Traversa <juliant@nescience.io>
2020-07-23 16:44:02 -0400

    trimming
    Co-Authored-By: Julian Traversa <62613746+jtraversa@users.noreply.github.com>

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -18,18 +18,6 @@ import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 import Divider from '@material-ui/core/Divider';
 
-import profile from "assets/img/faces/christian.jpg";
-
-import studio1 from "assets/img/examples/studio-1.jpg";
-import studio2 from "assets/img/examples/studio-2.jpg";
-import studio3 from "assets/img/examples/studio-3.jpg";
-import studio4 from "assets/img/examples/studio-4.jpg";
-import studio5 from "assets/img/examples/studio-5.jpg";
-import work1 from "assets/img/examples/olu-eletu.jpg";
-import work2 from "assets/img/examples/clem-onojeghuo.jpg";
-import work3 from "assets/img/examples/cynthia-del-rio.jpg";
-import work4 from "assets/img/examples/mariya-georgieva.jpg";
-import work5 from "assets/img/examples/clem-onojegaw.jpg";
 import MoneyVault from "assets/img/MoneyVault.png";
 import Compound from "assets/img/compound.png";
 import WateringMoney from "assets/img/WateringMoneySmall.png";
