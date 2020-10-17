9a3bdbe5ba99fd7d5d6a361e0a2ef89cc134b0e1
Julian Traversa <juliant@nescience.io>
2020-10-17 16:06:13 -0400

    Update MainLandingPage.js
    Co-Authored-By: Julian Traversa <62613746+JTraversa@users.noreply.github.com>

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -99,7 +99,7 @@ const NavLink2 = tw(NavLinkBase)`mr-12 lg:text-green-900 lg:hocus:text-green-300
 const LogoLink = tw(LogoLinkBase)`text-green-100 hocus:text-green-300`;
 const PrimaryLink = tw(PrimaryLinkBase)`shadow-raised lg:bg-primary-400 lg:hocus:bg-primary-500`;
 const PrimaryLink2 = tw(PrimaryLinkBase)`h-12 mt-12 shadow-raised lg:bg-primary-400 lg:hocus:bg-primary-500`;
-const SecondaryLink = tw(PrimaryLinkBase)`h-12 m-6 bg-gray-300 text-gray-800 hocus:bg-gray-400 hocus:text-gray-900`;
+const SecondaryLink = tw(PrimaryLinkBase)`h-12 ml-16 bg-gray-300 text-gray-800 hocus:bg-gray-400 hocus:text-gray-900`;
 
 export default ({
   features = null,
@@ -167,7 +167,7 @@ const logoLink = (
                 ))}
               </FeatureList>
               <Actions>
-                <PrimaryLink2 href={WhitepaperPDF} css={buttonRoundedCss} target="_blank">
+                <PrimaryLink2 href={WhitepaperPDF} target="_blank">
                   {primaryButtonText}
                 </PrimaryLink2>
                 <SecondaryLink href={secondaryButtonUrl} target="_blank">
